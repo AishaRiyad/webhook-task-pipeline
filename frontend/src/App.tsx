6ff98c359683c7a566af806af5f47c576c804c50
Aisha Riyad Abu Jeib <s12113150@stu.najah.edu>
2026-03-11 05:22:38 +0200

    add frontend dashboard project setup with vite and react

---
+++ frontend/src/App.tsx
@@ -0,0 +1,108 @@
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import DashboardPage from "./pages/DashboardPage";
+import PipelinesPage from "./pages/PipelinesPage";
+import JobsPage from "./pages/JobsPage";
+import ChainingPage from "./pages/ChainingPage";
+import SubscribersPage from "./pages/SubscribersPage";
+import WebhookTesterPage from "./pages/WebhookTesterPage";
+import JobDetailsPage from "./pages/JobDetailsPage";
+import MetricsPage from "./pages/MetricsPage";
+import NotificationsPage from "./pages/NotificationsPage";
+
+function PrivateRoute({ children }: { children: React.ReactNode }) {
+  const token = localStorage.getItem("frontend_access_token");
+  return token ? <>{children}</> : <Navigate to="/" replace />;
+}
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+
+      <Route
+        path="/dashboard"
+        element={
+          <PrivateRoute>
+            <DashboardPage />
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/pipelines"
+        element={
+          <PrivateRoute>
+            <PipelinesPage />
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/jobs"
+        element={
+          <PrivateRoute>
+            <JobsPage />
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/chaining"
+        element={
+          <PrivateRoute>
+            <ChainingPage />
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/subscribers"
+        element={
+          <PrivateRoute>
+            <SubscribersPage />
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/webhook-tester"
+        element={
+          <PrivateRoute>
+            <WebhookTesterPage />
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/jobs/:id"
+        element={
+          <PrivateRoute>
+            <JobDetailsPage />
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/metrics"
+        element={
+          <PrivateRoute>
+            <MetricsPage />
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/notifications"
+        element={
+          <PrivateRoute>
+            <NotificationsPage />
+          </PrivateRoute>
+        }
+      />
+    </Routes>
+  );
+}
